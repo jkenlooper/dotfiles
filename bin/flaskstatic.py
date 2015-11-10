3459589e49f35effe00e8aa161af274899c2ab05
Jake Hickenlooper <jake@weboftomorrow.com>
2015-11-10 11:08:51 -0700

    Add index directory support to flask static script

--- bin/flaskstatic.py
+++ bin/flaskstatic.py
@@ -2,19 +2,41 @@
 import os
 
 from werkzeug.serving import run_simple
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template_string, abort
 from livereload import Server, shell
 
 app = Flask(__name__, static_url_path=os.getcwd())
-#app.debug = True
+app.debug = True
+CWD = os.getcwd()
 
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
+directory_listing = """
+<a href="/{{ updirectory }}">
+/{{ updirectory }}
+</a><br>
+{{ directory }}<br>
+{% for file in files %}
+    <a href="{{ directory }}{{ file }}">{{ file }}</a><br>
+{% endfor %}
+"""
 
+@app.route('/', defaults={'filename': ''})
 @app.route('/<path:filename>')
 def send_it(filename):
-    return send_from_directory(os.getcwd(), filename)
+    absolute_filepath = os.path.join(CWD, filename)
+
+    if not os.path.exists(absolute_filepath):
+        return abort(404)
+
+    if os.path.isfile(absolute_filepath):
+        return send_from_directory(CWD, filename)
+
+    files = os.listdir(absolute_filepath)
+    directory = os.path.join('/', absolute_filepath[len(CWD)+1:])
+    if directory != '/':
+        directory = directory + '/'
+    updirectory = os.path.dirname(absolute_filepath[len(CWD)+1:])
+
+    return render_template_string(directory_listing, files=files, directory=directory, updirectory=updirectory)
 
 
 if __name__ == '__main__':
