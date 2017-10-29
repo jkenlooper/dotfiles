1fd0a1f2d56d6c9564a7e3c71dce7410fdbeb832
Jake Hickenlooper <jake@weboftomorrow.com>
2017-10-29 12:01:13 -0600

    Update vim set

--- startup.sh
+++ startup.sh
@@ -22,13 +22,13 @@ vim +PluginInstall +qall
 # for example:
 # pip install chill
 
-read -p "Enter git config user name: " STARTUP_GIT_CONFIG_USER_NAME
+read -p "Enter git config user name: [$(git config --global user.name)]" STARTUP_GIT_CONFIG_USER_NAME
 
 if test "$STARTUP_GIT_CONFIG_USER_NAME"; then
   git config --global --replace-all user.name $STARTUP_GIT_CONFIG_USER_NAME;
 fi
 
-read -p "Enter git config user email: " STARTUP_GIT_CONFIG_USER_EMAIL
+read -p "Enter git config user email: [$(git config --global user.email)]" STARTUP_GIT_CONFIG_USER_EMAIL
 
 if test "$STARTUP_GIT_CONFIG_USER_EMAIL"; then
   git config --global --replace-all user.email $STARTUP_GIT_CONFIG_USER_EMAIL;
