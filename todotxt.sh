5ca365c706c994a241aa4d61d60e5eb1f652ab79
Jake Hickenlooper <jake@weboftomorrow.com>
2017-10-29 10:42:22 -0600

    Fix dl link for todotxt

--- todotxt.sh
+++ todotxt.sh
@@ -7,7 +7,8 @@ cd $HOME/bin;
 if [ -d todo.txt_cli-2.10 ]; then
   echo 'todo.txt_cli already installed?';
 else
-  curl https://github.com/ginatrapani/todo.txt-cli/releases/download/v2.10/todo.txt_cli-2.10.tar.gz -O -L
+  curl https://github.com/todotxt/todo.txt-cli/releases/download/v2.10.0/todo.txt_cli-2.10.tar.gz -O -L
+
   tar -xf todo.txt_cli-2.10.tar.gz
   rm todo.txt_cli-2.10.tar.gz
 
