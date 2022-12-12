4cd15ca7bcf927929ba37a0ae447a76948575257
Jake Hickenlooper <jake@weboftomorrow.com>
2022-12-12 07:45:06 -0700

    Update timer.sh to not use todo.txt

--- bin/timer.sh
+++ bin/timer.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-trap "rm -f ${HOME}/.tmp_timer.txt" EXIT
+trap 'rm -f "$HOME/.tmp_timer.txt"' EXIT
 
 # This is a simple pomodoro timer that updates a tmp file with the remaining
 # pomodoro time.
 
 # Set to number of minutes to count down from.
-TIMER=${2-25}
+TIMER=${1-25}
 
-# Load the TASK variable with the todo text getting just the first line.
-TASK="$(todo.sh -p ls $1 | head -n1)"
-TASKNUMBER="$(echo $TASK | cut -f 1 -d ' ')"
-TASK=${TASK#${TASKNUMBER} }
-echo "Starting timer for task: ${TASK}"
+task="$(cat "$HOME/session.chat.txt" | xargs | grep -o -E '.{0,160}$')"
+echo "Enter task:"
+echo "[$task]"
+read -r updatetask
+if [ -n "$updatetask" ]; then
+  task="$updatetask"
+fi
+echo "Starting timer for task: ${task}"
 
-# Truncate it so the time left is at the end.
-#TASK=${TASK:0:34}
-
-printf "\n%s" "${TASK}" > ${HOME}/session.chat.txt
-while test $TIMER -gt 0; do
-  printf " :%s" "$TIMER" > ${HOME}/.tmp_timer.txt
-  TIMER=$(($TIMER-1))
+printf "\n%s" "${task}" > "$HOME/session.chat.txt"
+while test "$TIMER" -gt 0; do
+  printf " :%s" "$TIMER" > "$HOME/.tmp_timer.txt"
+  TIMER=$((TIMER-1))
   sleep 60
 done
-printf " :%s" "$TIMER" > ${HOME}/.tmp_timer.txt
+printf " :%s" "$TIMER" > "$HOME/.tmp_timer.txt"
 
 message_ui() {
   if hash terminal-notifier 2>/dev/null; then
@@ -34,5 +34,6 @@ message_ui() {
   tmux display-message "$1";
 }
 
-message_ui "Timer finished: ${TASK}"
+message_ui "Timer finished: ${task}"
 sleep 10
+printf "\n  %s" "$(date '+%H:%M')" >> "$HOME/session.chat.txt"
